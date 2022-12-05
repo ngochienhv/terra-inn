c5165fbdc27f6d9ee60293a2c7c502a37b95d8a7
HienNguyen <ngochien123hv@gmail.com>
2022-12-05 17:38:52 +0700

    ✨ feat: implement login screen and setup navigation, color theme

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/home/HomeScreen';
+import SigninScreen from './screens/signin/SigninScreen';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import { NavigatorParamList } from 'types/navigator';
+
+const Stack = createNativeStackNavigator<NavigatorParamList>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <Provider store={store}>
       <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Signin"
+            component={SigninScreen}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

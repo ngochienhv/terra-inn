202c8c28ef503e4b8a07a3f70181b99e7d836406
HienNguyen <ngochien123hv@gmail.com>
2022-12-08 23:45:39 +0700

    ✨ feat: implement profile, notification screens

--- screens/notification/NotificationScreen.tsx
+++ screens/notification/NotificationScreen.tsx
@@ -1,12 +1,46 @@
-import React from 'react';
-import { View, TextField, Text, Button } from 'react-native-ui-lib';
+import React, { useState } from 'react';
+import { FlatList, ScrollView } from 'react-native';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
+import { View, TextField, Text, Button, TabController } from 'react-native-ui-lib';
+import NotificationCard from '../../components/NotiCard/NotiCard';
+import { TERRA_COLOR } from '../../constants/theme/color';
 
-export default function NotificationScreen() {
+function NotificationComponents() {
+  const [selectedIndex, setSelectedIndex] = useState<number>(0);
+
   return (
-    <View flex paddingH-25 paddingT-120>
-      <Text blue50 text20>
-        Notification
-      </Text>
-    </View>
+    <TabController
+      items={[{ label: 'Từ ban quản lý' }, { label: 'Từ hệ thống' }]}
+      initialIndex={selectedIndex}
+      onChangeIndex={(index: number) => setSelectedIndex(index)}
+    >
+      <TabController.TabBar
+        enableShadows
+        activeBackgroundColor={TERRA_COLOR.PRIMARY[0]}
+        selectedLabelColor={TERRA_COLOR.PRIMARY[3]}
+        uppercase
+      />
+      <View flex>
+        <TabController.TabPage index={0}>{renderFirstPage()}</TabController.TabPage>
+        <TabController.TabPage index={1}>{renderFirstPage()}</TabController.TabPage>
+      </View>
+    </TabController>
   );
 }
+
+const renderFirstPage = () => {
+  return (
+    <View style={{ alignItems: 'center', marginTop: '5%' }}>
+      <FlatList
+        data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+        renderItem={() => <NotificationCard />}
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+      />
+    </View>
+  );
+};
+
+const NotificationScreen = gestureHandlerRootHOC(NotificationComponents);
+
+export default NotificationScreen;

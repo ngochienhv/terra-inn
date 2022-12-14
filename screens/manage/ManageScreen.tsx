2ec73f889377756966fa511d6c755a2d4822a6f6
HienNguyen <ngochien123hv@gmail.com>
2022-12-14 22:38:25 +0700

    feat: guest manage screens

--- screens/manage/ManageScreen.tsx
+++ screens/manage/ManageScreen.tsx
@@ -3,51 +3,42 @@ import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 import { View, TextField, Text, Button, TabController, Checkbox } from 'react-native-ui-lib';
 import NotificationCard from '../../components/NotiCard/NotiCard';
 import { TERRA_COLOR } from '../../constants/theme/color';
-import RequestCard from '../../components/RequestCard/RequestCard'
-import {FlatList, ImageBackground, StyleSheet, TextInput, Dimensions, ScrollView } from 'react-native';
+import MemberCard from '../../components/MemberCard/MemberCard';
+import {
+  FlatList,
+  ImageBackground,
+  StyleSheet,
+  TextInput,
+  Dimensions,
+  ScrollView,
+} from 'react-native';
 
 const users = [
   {
-      id: 1,
-      name: 'Hoàng Nhân Linh Chi',
-      image: {uri: 'https://scontent.fsgn13-2.fna.fbcdn.net/v/t39.30808-6/264503003_4487878974650832_6897324498459401338_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5cd70e&_nc_ohc=ttmXErzQ6DwAX-MFtcW&_nc_ht=scontent.fsgn13-2.fna&oh=00_AfBLA51eeg-rVu8MWWAZZ0Ay4myb6b4BAik-WNp5vE6Hng&oe=63960044'},
-      time: '2 giờ trước',
-      inn: 'An Khang',
-      room: '02',
-      request: 'Đăng ký thuê phòng',
-      status: 'Chưa hoàn thành',
+    id: 1,
+    name: 'Hoàng Nhân Linh Chi',
+    image:
+      'https://scontent.fsgn13-2.fna.fbcdn.net/v/t39.30808-6/264503003_4487878974650832_6897324498459401338_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5cd70e&_nc_ohc=ttmXErzQ6DwAX-MFtcW&_nc_ht=scontent.fsgn13-2.fna&oh=00_AfBLA51eeg-rVu8MWWAZZ0Ay4myb6b4BAik-WNp5vE6Hng&oe=63960044',
+    phone: '0987654321',
+    age: 19,
   },
   {
-      id: 2,
-      name: 'Lương Nguyễn Thị Thu Hoài',
-      image: {uri: 'https://scontent.fsgn13-3.fna.fbcdn.net/v/t39.30808-6/317838285_131700689707738_3868177724488407947_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5cd70e&_nc_ohc=GB4kT5OnYzkAX90XQPp&_nc_ht=scontent.fsgn13-3.fna&oh=00_AfBvxpo2VQvMINWDiAfvGOSkNgNMBl2LBXUKHd7GHM0-1w&oe=63962DA2'},
-      time: '2 giờ trước',
-      inn: 'An Khang',
-      room: '02',
-      request: 'Đăng ký thuê phòng',
-      status: 'Đã hoàn thành',
+    id: 2,
+    name: 'Hoàng Nhân Linh Chi',
+    image:
+      'https://scontent.fsgn13-2.fna.fbcdn.net/v/t39.30808-6/264503003_4487878974650832_6897324498459401338_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5cd70e&_nc_ohc=ttmXErzQ6DwAX-MFtcW&_nc_ht=scontent.fsgn13-2.fna&oh=00_AfBLA51eeg-rVu8MWWAZZ0Ay4myb6b4BAik-WNp5vE6Hng&oe=63960044',
+    phone: '0987654321',
+    age: 19,
   },
   {
     id: 3,
-    name: 'Nguyễn Phương Lan Anh',
-    image: {uri: 'https://scontent.fsgn4-1.fna.fbcdn.net/v/t39.30808-6/315142649_6154953301231904_8123856781363846108_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5cd70e&_nc_ohc=M0NPBP__LKkAX8Lt5o4&tn=AGlP9pozc-tpxuWV&_nc_ht=scontent.fsgn4-1.fna&oh=00_AfCzQd_90VIFa-DaDwb7mhjloPRFsfVNBw45x0kpNsq_Dw&oe=6396EA34'},
-    time: '2 giờ trước',
-    inn: 'An Khang',
-    room: '02',
-    request: 'Đăng ký thuê phòng',
-      status: 'Chờ thanh toán',
+    name: 'Hoàng Nhân Linh Chi',
+    image:
+      'https://scontent.fsgn13-2.fna.fbcdn.net/v/t39.30808-6/264503003_4487878974650832_6897324498459401338_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5cd70e&_nc_ohc=ttmXErzQ6DwAX-MFtcW&_nc_ht=scontent.fsgn13-2.fna&oh=00_AfBLA51eeg-rVu8MWWAZZ0Ay4myb6b4BAik-WNp5vE6Hng&oe=63960044',
+    phone: '0987654321',
+    age: 19,
   },
-  {
-    id: 4,
-    name: 'Nguyễn Phương Lan Anh',
-    image: {uri: 'https://scontent.fsgn4-1.fna.fbcdn.net/v/t39.30808-6/315142649_6154953301231904_8123856781363846108_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5cd70e&_nc_ohc=M0NPBP__LKkAX8Lt5o4&tn=AGlP9pozc-tpxuWV&_nc_ht=scontent.fsgn4-1.fna&oh=00_AfCzQd_90VIFa-DaDwb7mhjloPRFsfVNBw45x0kpNsq_Dw&oe=6396EA34'},
-    time: '2 giờ trước',
-    inn: 'An Khang',
-    room: '02',
-    request: 'Đăng ký thuê phòng',
-      status: 'Chưa hoàn thành',
-  },
-  ]
+];
 
 function ManageComponents() {
   const [selectedIndex, setSelectedIndex] = useState<number>(0);
@@ -76,41 +67,59 @@ function ManageComponents() {
 const renderFirstPage = () => {
   return (
     <ScrollView>
-      <View style={styles.container}>
+      <View>
         {users.map((user) => (
-          <RequestCard 
-            key = {user.id}
-            name = {user.name}
-            request = {user.request}
-            inn = {user.inn}
-            room = {user.room}
-            time = {user.time}
-            image = {user.image}
-            status = {user.status}/>
+          <MemberCard key={user.id} {...user} />
         ))}
-          
       </View>
-    {/* <View style={{padding: 50 }} /> */}
     </ScrollView>
   );
 };
+
+const Row = ({ title, content }: { title: string; content: string | number }) => (
+  <View style={styles.rowContainer}>
+    <Text color={TERRA_COLOR.GRAY[3]}>{title}</Text>
+    <Text color={TERRA_COLOR.PRIMARY[4]}>{content}</Text>
+  </View>
+);
 
 const renderSecondPage = () => {
   return (
     <ScrollView>
       <View style={styles.container}>
-        {users.map((user) => (
-          <RequestCard 
-            key = {user.id}
-            name = {user.name}
-            request = {user.request}
-            inn = {user.inn}
-            room = {user.room}
-            time = {user.time}
-            image = {user.image}
-            status = {user.status}/>
-        ))}
-          
+        <Row title="Giá thuê" content="2000000 đ" />
+        <Row title="Giá điện" content="2500 đ/kWh" />
+        <Row title="Giá nước" content="14000 đ/kWh" />
+        <View style={{ padding: 10 }}>
+          <View
+            style={{
+              ...styles.rowContainerSpec,
+              borderTopRightRadius: 15,
+              borderTopLeftRadius: 15,
+            }}
+          >
+            <Text color={TERRA_COLOR.GRAY[3]}>Phí dịch vụ</Text>
+            <Text color={TERRA_COLOR.PRIMARY[4]}>200000 đ</Text>
+          </View>
+          <View style={styles.rowContainerSpec}>
+            <Text color={TERRA_COLOR.GRAY[3]}>Tiền rác</Text>
+            <Text color={TERRA_COLOR.PRIMARY[4]}>50000 đ</Text>
+          </View>
+          <View style={styles.rowContainerSpec}>
+            <Text color={TERRA_COLOR.GRAY[3]}>Tiền wifi</Text>
+            <Text color={TERRA_COLOR.PRIMARY[4]}>100000 đ</Text>
+          </View>
+          <View
+            style={{
+              ...styles.rowContainerSpec,
+              borderBottomLeftRadius: 15,
+              borderBottomRightRadius: 15,
+            }}
+          >
+            <Text color={TERRA_COLOR.GRAY[3]}>Tiền giữ xe</Text>
+            <Text color={TERRA_COLOR.PRIMARY[4]}>50000 đ</Text>
+          </View>
+        </View>
       </View>
     </ScrollView>
   );
@@ -119,14 +128,13 @@ const renderSecondPage = () => {
 const renderThirdPage = () => {
   return (
     <ScrollView>
-      <View style={styles.container}>
-      <Text text80 color={TERRA_COLOR.GRAY[3]}>
-            Các dịch vụ của phòng
-          </Text>
-      <Checkbox value={true} label = 'wifi' onValueChange={() => console.log('value changed')}/>
-      <Checkbox value={true} label = 'dich vu' onValueChange={() => console.log('value changed')}/>
-      <Checkbox value={true} label = 'gui xe' onValueChange={() => console.log('value changed')}/>
-          
+      <View>
+        <Text text65 color={TERRA_COLOR.GRAY[3]}>
+          Các dịch vụ của phòng
+        </Text>
+        <Checkbox value={false} label="Wifi" color={TERRA_COLOR.DEFAULT[4]} margin-5 />
+        <Checkbox value={false} label="Dọn rác hằng ngày" color={TERRA_COLOR.DEFAULT[4]} margin-5 />
+        <Checkbox value={false} label="Trông giữ xe" color={TERRA_COLOR.DEFAULT[4]} margin-5 />
       </View>
     </ScrollView>
   );
@@ -138,11 +146,26 @@ export default ManageScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flexGrow: 1,
     flexDirection: 'column',
     justifyContent: 'flex-start',
     margin: 10,
     borderRadius: 20,
+    backgroundColor: TERRA_COLOR.PRIMARY[2],
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: '5%',
+    margin: '2%',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderRadius: 15,
+  },
+  rowContainerSpec: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: '5%',
+    alignItems: 'center',
     backgroundColor: 'white',
   },
 });
